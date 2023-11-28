f56f208c36a0ed7a4f7fcbc2490e89af155e956f
AlphaCloudX <66267343+AlphaCloudX@users.noreply.github.com>
2023-11-27 21:57:04 -0500

    Update givenA3.h

--- test6/givenA3.h
+++ test6/givenA3.h
@@ -3,16 +3,15 @@
 #define NUMBER_PROFS 1
 #define NUMBER_COURSES 1
 #define MAX_STR 100
-static char coursesToCheck[][MAX_STR] ={THIS_IS_ONE_HUNDRED_CHARACTERS_LONG_THIS_IS_ONE_HUNDRED_CHARACTERS_LONG_THIS_IS_
-ONE_HUNDRED_CHARAC, };
+static char coursesToCheck[][MAX_STR] ={"THIS_IS_ONE_HUNDRED_CHARACTERS_LONG_THIS_IS_ONE_HUNDRED_CHARACTERS_LONG_THIS_IS_ONE_HUNDRED_CHARAC"};
 
-static int courseIDs[NUMBER_COURSES] ={100, };
+static int courseIDs[NUMBER_COURSES] ={100};
 
-static char profsToCheck[NUMBER_PROFS][MAX_STR] = {High Mile Club, };
+static char profsToCheck[NUMBER_PROFS][MAX_STR] = {"High Mile Club"};
 
 static int profsTeachingCourses[NUMBER_PROFS][NUMBER_COURSES] ={{100}};
 
-static int task6Validation[NUMBER_COURSES] ={1,};
+static int task6Validation[NUMBER_COURSES] ={1};
 
 #define avg 1.00
 
