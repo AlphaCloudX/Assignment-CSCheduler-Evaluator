134e1914b1e524f0c43aa6e44c18c368b129deb2
AlphaCloudX <66267343+AlphaCloudX@users.noreply.github.com>
2023-11-27 21:17:05 -0500

    Add files via upload

---
+++ RunProgram.sh
@@ -0,0 +1,92 @@
+echo 'What is your methods file called(including the .c)'
+read filename
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/OutputAssignment3.c'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test1/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test1/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test1/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test2/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test2/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test2/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test3/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test3/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test3/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test4/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test4/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test4/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test5/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test5/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test5/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test6/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test6/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test6/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test7/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test7/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test7/givenA3.h'
+clear
+gcc '-Walls -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out'
+./ASSIGNMENT3TESTCASE.out
+echo 'Press Enter To Continue'
+rm 'ASSIGNMENT3TESTCASE.out'
+rm 'courses.txt'
+rm 'data.txt'
+rm 'givenA3.h'
+rm 'OutputAssignment3.c'
+read blah
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/defaults/courses.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/defaults/data.txt'
+wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/defaults/givenA3.h'
+echo 'Thanks for using the autograder'
