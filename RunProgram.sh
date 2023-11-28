87884572af0cfb60638a3d1d3ddc4ec270ba5798
AlphaCloudX <66267343+AlphaCloudX@users.noreply.github.com>
2023-11-27 23:41:55 -0500

    Update RunProgram.sh

--- RunProgram.sh
+++ RunProgram.sh
@@ -9,6 +9,9 @@ wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evalua
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test1/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test1/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test1/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -21,6 +24,9 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test2/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test2/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test2/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -33,6 +39,9 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test3/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test3/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test3/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -45,6 +54,9 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test4/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test4/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test4/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -57,6 +69,9 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test5/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test5/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test5/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -69,6 +84,9 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test6/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test6/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test6/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -81,6 +99,9 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test7/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test7/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/test7/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 clear
 gcc -Wall -std=c99 $filename OutputAssignment3.c -o ASSIGNMENT3TESTCASE.out
 ./ASSIGNMENT3TESTCASE.out
@@ -94,4 +115,7 @@ read blah
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/defaults/courses.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/defaults/data.txt'
 wget 'https://raw.githubusercontent.com/AlphaCloudX/Assignment-CSCheduler-Evaluator/main/defaults/givenA3.h'
+tr -d '\15\32' < courses.txt > courses.txt
+tr -d '\15\32' < data.txt > data.txt
+tr -d '\15\32' < givenA3 > givenA3
 echo 'Thanks for using the autograder'
